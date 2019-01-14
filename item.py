f9ac97c91f0f1c4f56f428a094b54103a7a4ee04
Chalbi mouhamed amine <mouhamedamine.chalbi@supcom.tn>
2019-01-14 18:38:57 +0100

    Update item.py

--- item.py
+++ item.py
@@ -10,7 +10,6 @@ class Item(Resource):
     parser=reqparse.RequestParser()
     parser.add_argument('price',type=float,required=True,help="this field can't be left blank")
     
-    @jwt_required()
     def get(self,name):
         connection=sqlite3.connect("data.db")
         cursor=connection.cursor()
