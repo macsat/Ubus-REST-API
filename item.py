47e35df1b90e7cebdc911980b8a54ec6377edde7
Chalbi mouhamed amine <mouhamedamine.chalbi@supcom.tn>
2019-01-14 21:00:51 +0100

    Update item.py

--- item.py
+++ item.py
@@ -20,7 +20,7 @@ class Item(Resource):
         connection.close()
         
         if row:
-            return {"trip":{"deperture_station":row[1] ,"arrival_station":row[2],"price":row[3]}}
+            return {"trip":{"id":row[0],"deperture_station":row[1] ,"arrival_station":row[2],"price":row[3]}}
         return {"message":"no trips for this place"},404
     
     def post(self,name):
